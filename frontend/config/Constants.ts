dfd26d1116b6ea94ba387c5944f23de6b08e0d94
yashUcr773 <yagga004@ucr.edu>
2024-02-21 22:17:46 -0800

    Update FE routes

--- frontend/config/Constants.ts
+++ frontend/config/Constants.ts
@@ -11,8 +11,18 @@ export const CONSTANTS = {
         LOGOUT: "/auth/signout"
     },
     USER: {
-        GETALL: "/users",
-        GETBYID: "/users/"
+        GET_ALL: "/ap1/v1/users",
+        GET_BY_ID: "/ap1/v1/users/"
+    },
+    MESSAGE: {
+        GET_MESSAGES: "/ap1/v1/messages/",
+        CREATE_MESSAGE: "/ap1/v1/messages/create"
+    },
+    CHAT: {
+        CREATE_CHAT: "/ap1/v1/chats/create",
+        FIND_CHAT_USER: "/ap1/v1/user/chats/",
+        FIND_CHAT_BY_MEMBERS: "/ap1/v1/chats/",
+        FIND_CHAT_BY_ID: "/ap1/v1/chats/"
     }
 }
 
